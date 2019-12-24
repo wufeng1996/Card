df69b5228571ecee70ebfd32a1efe352b9fbaffa
wufeng1996 <895819764@qq.com>
2019-12-24 15:14:48 +0800

    Updates

---
+++ precache-manifest.a3d3e438e160be0d7471cd22cdc63bd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44091f5fd6d61faec9b63c5d74055f3c",
+    "url": "/christmas-card/index.html"
+  },
+  {
+    "revision": "7789befa26da9ac830cc",
+    "url": "/christmas-card/static/css/main.80efff49.chunk.css"
+  },
+  {
+    "revision": "5471337c4c7fe4aa4ac8",
+    "url": "/christmas-card/static/js/2.7e208509.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/christmas-card/static/js/2.7e208509.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7789befa26da9ac830cc",
+    "url": "/christmas-card/static/js/main.a2a1faa0.chunk.js"
+  },
+  {
+    "revision": "e1a72e9f2f41ef86a6d8",
+    "url": "/christmas-card/static/js/runtime-main.d3c651f5.js"
+  },
+  {
+    "revision": "fe1a2d59a53b151f36cad517618b7f58",
+    "url": "/christmas-card/static/media/Respective-VP6y.fe1a2d59.ttf"
+  },
+  {
+    "revision": "59e50f7d0e99f3374541f064726b5fcf",
+    "url": "/christmas-card/static/media/exmouth_.59e50f7d.ttf"
+  },
+  {
+    "revision": "ff7e629702d1790226095430f58aa673",
+    "url": "/christmas-card/static/media/liukai.ff7e6297.ttf"
+  }
+]);
